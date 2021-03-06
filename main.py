9795cb49837a2fffb63a386f7d615d6d24c97f69
Nik <nn494@yandex.ru>
2021-03-06 14:57:26 +0300

    Another task completed.

--- main.py
+++ main.py
@@ -213,5 +213,26 @@ def planet_choice(planet_name):
                 </div>'''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''<link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossorigin="anonymous">
+               <h1>Результаты отбора</h1>
+               <div class="alert alert-primary" role="alert">
+                Претендента на участие в миссии {nickname}:  
+               </div>
+               <div class="alert alert-secondary" role="alert">
+                Поздравляем! Ваш рейтинг после {level} этапа отбора 
+               </div>
+               <div class="alert alert-success" role="alert">
+                составляет {rating}
+               </div>
+               <div class="alert alert-warning" role="alert">
+                 Желаем удачи!
+               </div>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
